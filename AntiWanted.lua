35884b4950b77fbc262506c05cfa65cd1a1204ca
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-01-24 23:55:00 +0000

    Create AntiWanted.lua

---
+++ AntiWanted.lua
@@ -0,0 +1,3 @@
+--legit states it in it's name. execute when u spawn in and when ur innocent
+
+game:GetService("Workspace").RayIgnore[game:GetService("Players").LocalPlayer.Name].Head.PlayerDisplay.Wanted:Destroy()
